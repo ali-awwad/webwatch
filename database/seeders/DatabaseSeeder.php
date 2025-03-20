75baba35737fbe81cdd811346e6346bf159a22ac
AliAwwad <AliAwwad1@gmail.com>
2025-03-20 17:47:23 +0400

    b4 ai

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Ali Awwad',
             'email' => 'a.awwad@outlook.com',
+            'password' => Hash::make('my@C0mplexP@ssw0rd@2231'),
         ]);
     }
 }
